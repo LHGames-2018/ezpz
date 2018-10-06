cbf31183e1cb2041bc220905d34107c66a8ef29b
nlangheit <nicolas.langheit@hotmail.com>
2018-10-06 11:20:27 -0400

    deplace sur lla maison

--- bot/bot.py
+++ bot/bot.py
@@ -21,9 +21,10 @@ class Bot:
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
 
+        if (gameMap.getTileAt(self.Position) != 2):
+            move = create_move_action(Point(1, 0))
 
-
-        return create_move_action(Point(-1, 0))
+        return move
 
     def after_turn(self):
         """
