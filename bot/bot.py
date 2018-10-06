3649e3e21475e6827a23fca73a5c3b2695611434
nlangheit <nicolas.langheit@hotmail.com>
2018-10-06 11:39:42 -0400

    maison

--- bot/bot.py
+++ bot/bot.py
@@ -22,7 +22,7 @@ class Bot:
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
 
         if (gameMap.getTileAt(self.Position) != 2):
-            move = create_move_action(Point(1, 0))
+            move = create_move_action(Point(-1, 0))
 
         return move
 
