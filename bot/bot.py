84805d4584716f2c561d0e82bafeb1afc6f8353b
nlangheit <nicolas.langheit@hotmail.com>
2018-10-06 18:29:13 -0400

    lit

--- bot/bot.py
+++ bot/bot.py
@@ -60,6 +60,8 @@ class Bot:
                 return create_move_action(Point(-1, 0))
 
         def trouverObjet( type):
+            if self.PlayerInfo.CarriedResources == self.PlayerInfo.CarryingCapacity:
+                return RetourMaison()
             position = self.PlayerInfo.Position
             if gameMap.getTileAt(Point(position.x + 1, position.y)) == TileContent(4):
                 return create_collect_action(Point(1, 0))
@@ -81,32 +83,22 @@ class Bot:
                 return create_attack_action(Point(0, -1))
 
 
-            if self.PlayerInfo.CarriedResources == self.PlayerInfo.CarryingCapacity:
-                return RetourMaison()
-            else:
-                position = self.PlayerInfo.Position
 
 
-
-                for y in range(position.y - 10, position.y + 10):
-                    for x in range(position.x - 10, position.x + 10):
-                        if gameMap.getTileAt(Point(x, y)) == TileContent(type):
-
-                            if position.x-x == 1 and position.y-y == 0 :
-                                return create_collect_action(Point(-1, 0))
-
-                            elif position.x-x == -1 and position.y-y == 0:
-                                return create_collect_action(Point(1, 0))
-
-                            if position.y-y == 1 and position.x-x == 0:
-                                return create_collect_action(Point(0, -1))
-
-                            elif position.y-y == -1 and position.x-x == 0:
-                                return create_collect_action(Point(0, 1))
-
-                            direction = position - Point(x, y)
-                            return faireMouvement(direction)
-
+            position = self.PlayerInfo.Position
+            for y in range(position.y - 10, position.y + 10):
+                for x in range(position.x - 10, position.x + 10):
+                    if gameMap.getTileAt(Point(x, y)) == TileContent(type):
+                        if position.x-x == 1 and position.y-y == 0 :
+                            return create_collect_action(Point(-1, 0))
+                        elif position.x-x == -1 and position.y-y == 0:
+                            return create_collect_action(Point(1, 0))
+                        if position.y-y == 1 and position.x-x == 0:
+                            return create_collect_action(Point(0, -1))
+                        elif position.y-y == -1 and position.x-x == 0:
+                            return create_collect_action(Point(0, 1))
+                        direction = position - Point(x, y)
+                        return faireMouvement(direction)
         def bouger():
             direction = self.PlayerInfo.Position - (self.PlayerInfo.HouseLocation - Point(-5, 3))
 
